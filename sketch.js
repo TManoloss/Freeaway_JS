233f34d29d2ec5729f151ab7290c67ee05b197e3
TManoloss <manoelfelipe90@gmail.com>
2023-03-01 00:46:45 -0300

    movimento carro e ator

--- sketch.js
+++ sketch.js
@@ -1,6 +1,13 @@
 let imagemDaEstrada
+let imagemDaEstrada
 let imagemDoAtor
 let imagemCarro
+
+//carro
+let xCarro = 600
+
+//ator
+let yAtor = 365
 
 function preload() {
   imagemDaEstrada = loadImage('imagens/estrada.png')
@@ -13,6 +20,29 @@ function setup() {
 
 function draw() {
   background(imagemDaEstrada)
-  image(imagemDoAtor, 100, 365, 30, 30)
-  image(imagemCarro, 420, 40, 50, 40)
+  mostraAtor()
+  mostraCarro()
+  movimentaCarro()
+  movimentaAtor()
+}
+
+function mostraAtor() {
+  image(imagemDoAtor, 100, yAtor, 30, 30)
+}
+
+function mostraCarro() {
+  image(imagemCarro, xCarro, 40, 50, 40)
+}
+
+function movimentaCarro() {
+  xCarro -= 2
+}
+
+function movimentaAtor() {
+  if (keyIsDown(UP_ARROW)) {
+    yAtor -= 2
+  }
+  if (keyIsDown(DOWN_ARROW)) {
+    yAtor += 2
+  }
 }
