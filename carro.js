115cd216b18097acdf5fcf95d28081f5000bcd1f
TManoloss <manoelfelipe90@gmail.com>
2023-03-01 03:36:30 -0300

    add mais carros

--- carro.js
+++ carro.js
@@ -1,12 +1,28 @@
 //código do carro
 
+//carro 1
 let xCarro = 600
 let yCarro = 40
+let velocidadeCarro = 2
+
+// carro 2
+let xCarro2 = 600
+let yCarro2 = 95
+let velocidadeCarro2 = 2.5
+
+// carro 3
+let xCarro3 = 600
+let yCarro3 = 148
+let velocidadeCarro3 = 3.2
 
 function mostraCarro() {
   image(imagemCarro, xCarro, yCarro, 50, 40)
+  image(imagemCarro2, xCarro2, yCarro2, 50, 40)
+  image(imagemCarro3, xCarro3, yCarro3, 50, 40)
 }
 
 function movimentaCarro() {
-  xCarro -= 2
+  xCarro -= velocidadeCarro
+  xCarro2 -= velocidadeCarro2
+  xCarro3 -= velocidadeCarro3
 }
