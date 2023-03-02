81daa1543d0e1e167a37670c02d5c7b780f894fd
TManoloss <manoelfelipe90@gmail.com>
2023-03-02 00:43:09 -0300

    Melhorando código

--- carro.js
+++ carro.js
@@ -1,40 +1,27 @@
 //código do carro
-
-//carro 1
-let xCarro = 600
-let yCarro = 40
-let velocidadeCarro = 2
-
-// carro 2
-let xCarro2 = 600
-let yCarro2 = 95
-let velocidadeCarro2 = 2.5
-
-// carro 3
-let xCarro3 = 600
-let yCarro3 = 148
-let velocidadeCarro3 = 3.2
+let xCarros = [600, 600, 600]
+let yCarros = [40, 96, 148]
+let velocidadeCarros = [2, 2.5, 3.2]
 
 function mostraCarro() {
-  image(imagemCarro, xCarro, yCarro, 50, 40)
-  image(imagemCarro2, xCarro2, yCarro2, 50, 40)
-  image(imagemCarro3, xCarro3, yCarro3, 50, 40)
+  for (let i = 0; i < imagemCarros.length; i++) {
+    image(imagemCarros[i], xCarros[i], yCarros[i], 50, 40)
+  }
 }
-
 function movimentaCarro() {
-  xCarro -= velocidadeCarro
-  xCarro2 -= velocidadeCarro2
-  xCarro3 -= velocidadeCarro3
+  for (i = 0; i < imagemCarros.length; i++) {
+    xCarros[i] -= velocidadeCarros[i]
+  }
 }
 
 function voltaPosicaoInicialDoCarro() {
-  if (xCarro < -50) {
-    xCarro = 600
+  for (i = 0; i < imagemCarros.length; i++) {
+    if (passouTodaATela(xCarros[i])) {
+      xCarros[i] = 600
+    }
   }
-  if (xCarro2 < -50) {
-    xCarro2 = 600
-  }
-  if (xCarro3 < -50) {
-    xCarro3 = 600
-  }
+}
+
+function passouTodaATela(xCarros) {
+  return xCarros < -50
 }
